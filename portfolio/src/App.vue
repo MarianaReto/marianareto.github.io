992cdb015f0f88f5a18ec472663a4907afd827d6
Mariana Reto <mariana.reto@cgi.com>
2023-12-19 15:50:37 +0100

    vue-frontend: Added vuetify dependency

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <NavigationBar/>
-  <router-view/>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
--- portfolio/src/components/Navbar.vue
+++ portfolio/src/components/Navbar.vue
@@ -1,4 +1,16 @@
 <template>
+  <header class="navbar">
+    <div class="navbar-left">
+      <span class="name"> Mariana </span>
+    </div>
+    <div class="navbar-right">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/skills">Skills</router-link>
+      <router-link to="/works">Works</router-link>
+      <router-link to="/contact">Contact Me</router-link>
+    </div>
+  </header>
   <nav>
     <ul>
       <li>
@@ -46,5 +58,32 @@ li {
 a {
   color: white;
   text-decoration: none;
+}
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  background-color: #333;
+  color: white;
+}
+
+.navbar-left {
+  display: flex;
+  align-items: center;
+}
+
+.name {
+  font-size: 1.5rem;
+}
+
+.navbar-right {
+  display: flex;
+  align-items: center;
+}
+
+.navbar-right router-link {
+  color: white;
+  text-decoration: none;
+  margin-left: 10px;
 }
 </style>
