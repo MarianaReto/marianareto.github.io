4134227d260b7ab678bb4a1193ab875ec8936a7a
Mariana Reto <mariana.reto@cgi.com>
2023-12-19 11:41:12 +0100

    vue-frontend: Initial commit for vue boilerplate

---
+++ portfolio/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <NavigationBar/>
+  <router-view/>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import NavigationBar from "@/components/Navbar.vue";
+
+export default {
+  name: 'App',
+  components: {
+    NavigationBar,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ portfolio/src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/about">About</router-link>
+      </li>
+      <li>
+        <router-link to="/skills">Skills</router-link>
+      </li>
+      <li>
+        <router-link to="/works">Works</router-link>
+      </li>
+      <li>
+        <router-link to="/contact">Contact Me</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavigationBar',
+};
+</script>
+
+<style scoped>
+nav {
+  background-color: #333;
+  color: white;
+  padding: 10px;
+}
+
+ul {
+  list-style-type: none;
+  display: flex;
+  justify-content: space-around;
+}
+
+li {
+  margin: 0;
+}
+
+a {
+  color: white;
+  text-decoration: none;
+}
+</style>
