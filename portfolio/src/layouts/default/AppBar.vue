6f5923f6dcedd541003bf8f339bdc08e399cf279
Mariana <marianareto99@gmail.com>
2023-12-19 20:39:29 +0100

    vue-frontend: reworked navigation bar

--- portfolio/src/layouts/default/AppBar.vue
+++ portfolio/src/layouts/default/AppBar.vue
@@ -1,13 +1,35 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-4" />
-
-      Base Preset
+  <v-app-bar flat color="teal-darken-2">
+    <v-app-bar-title >
+      Mariana Reto
     </v-app-bar-title>
+    <v-tabs fixed-tabs bg-color="teal-darken-2">
+      <v-tab>
+        <router-link to="/">Home</router-link>
+      </v-tab>
+      <v-tab>
+        <router-link to="/about">About</router-link>
+      </v-tab>
+      <v-tab>
+        <router-link to="/skills">Skills</router-link>
+      </v-tab>
+      <v-tab>
+        <router-link to="/works">Works</router-link>
+      </v-tab>
+      <v-tab>
+        <router-link to="/contact">Contact Me</router-link>
+      </v-tab>
+    </v-tabs>
   </v-app-bar>
 </template>
 
 <script setup>
   //
 </script>
+
+<style>
+a {
+  color: white;
+  text-decoration: none;
+}
+</style>
--- portfolio/src/views/Home.vue
+++ portfolio/src/views/Home.vue
@@ -1,9 +1,7 @@
 <template>
-  <navigation-bar/>
   <HelloWorld/>
 </template>
 
 <script setup>
-import HelloWorld from '@/components/HelloWorld.vue'
-import NavigationBar from "@/components/Navbar.vue";
+import HelloWorld from '@/components/Home.vue'
 </script>
